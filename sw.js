220be12dcde619a2e77eb0699472c1623f3e0058
HongKongPfui2021 <95980454+HongKongPfui2021@users.noreply.github.com>
2026-07-08 11:43:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "urlaubscheckliste-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Beim Installieren: eigene Dateien in den Cache legen
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Alte Caches beim Aktivieren aufräumen
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Cache-first für eigene Dateien, Network-first für alles andere (z.B. html2pdf.js CDN)
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const isOwnAsset = url.origin === self.location.origin;
+
+  if (isOwnAsset) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+});
